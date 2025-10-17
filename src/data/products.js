938e49e1ae2f9d59050ed09f3fe6213c6c6bceb8
aimesananton <aimesananton@gmail.com>
2025-10-17 10:51:41 -0300

    cambio de catalogo

--- src/data/products.js
+++ src/data/products.js
@@ -1,14 +1,13 @@
 // mantuve los mismos nombres y precios. Las imágenes deben estar en public/assets/
 const BDSimu = [
-  { img: "/babyñagon.jpg", nombre: "babyñagon", precio: 1 },
-  { img: "/healspirit.jpg", nombre: "heal spirit", precio: 5 },
-  { img: "/babydragon.jpg", nombre: "baby dragon", precio: 10 },
-  { img: "/elpekas.jpg", nombre: "el pekas", precio: 15 },
-  { img: "/icespirit.jpg", nombre: "ice spirit", precio: 20 },
-  { img: "/log.jpg", nombre: "log", precio: 25 },
-  { img: "/minion.jpg", nombre: "minion", precio: 30 },
-  { img: "/minipekka.jpg", nombre: "mini pekka", precio: 35 },
-  { img: "/sybau.jpg", nombre: "mimimi", precio: 40 }
+  { img: "/himemori luna.jpg ", nombre: "Himemori Luna Birthday 2025", precio: 1 },
+  { img: "/birthday 2025.jpg", nombre: "Inugami Korone Birthday 2025", precio: 5 },
+  { img: "/campsite.jpg", nombre: "AZKi ASMR Voice Pack: Campsite Respite", precio: 10 },
+  { img: "/KO brithday.jpg", nombre: "Kureiji Ollie Birthday 2025", precio: 15 },
+  { img: "MC 5ht.jpg", nombre: "Mori Calliope 5th Anniversary", precio: 20 },
+  { img: "/PR dayofmera.jpg", nombre: "Pavolia Reine ~Day of MERAKyats~ 2025", precio: 25 },
+  { img: "/te seat beside mine.jpg", nombre: "hololive The Seat Beside Mine Voice", precio: 30 },
+  { img: "/yuzuki choco.jpg", nombre: "Yuzuki Choco Birthday Celebration 2025", precio: 35 },
 ];
 
 export default BDSimu;
