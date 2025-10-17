30e2b58a52a026b1c979d7f85c645fbf1e338906
セカンド <Segudg@gmail.com>
2025-10-17 14:56:01 -0300

    Small changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import CartProvider from "./context/CartProvider";
-// import "./styles.css"; // asumiendo que pegas tu style.css aquí
+
 
 const root = createRoot(document.getElementById("root"));
 root.render(
